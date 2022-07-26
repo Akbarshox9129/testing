509e6fc2f88482e8c69606c51b5063dffd0d1db1
Akbarshox <masterw9129@gmail.com>
2022-07-26 11:00:32 +0500

    'ikkinchi_saqlanish'

--- 49-algoritms.py
+++ 49-algoritms.py
@@ -13,17 +13,8 @@ def selectionsort(nums):
             if nums[j]<nums[min]:
                 min=j
         nums[min],nums[i]=nums[i],nums[min]
-# Insertion Sort
-def insertionsort(nums):
-    for i in range(1,len(nums)):
-        a=nums[i]
-        j=i-1
-        while j>=0 and nums[j]>a:
-            nums[j+1]=nums[j]
-            j-=1
-        nums[j+1]=a
 sonlar=[4,9,1,6,7,3,4]
 #bubblesort(sonlar)
 #selectionsort(sonlar)
-insertionsort(sonlar)
+#insertionsort(sonlar)
 print(sonlar)
